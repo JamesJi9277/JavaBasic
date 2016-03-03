d75a0b80a0c980b8f91e5de250178120798c39b8
JamesJi9277 <jiqi@gwu.edu>
2016-03-03 13:42:02 -0500

    update

--- springExample.java
+++ springExample.java
@@ -1,3 +1,22 @@
+原始方法
+新建两个类，一个是triangle一个是circle
+Triangle myTriangle = new Triangle();
+myTriangle.draw();
+
+Circle myCircle = new Circle();
+myCircle.draw();
+
+using polymorphism
+新建一个shape interface或者abstract class，circle和triangle两个类要去
+override shape里面的draw方法
+
+Shape shape = new Triangle();
+shape.draw();//draw a triangle
+
+Shape shape = new Circle();
+shape.draw();//draw a circle
+
+
 drawing class
 protected class Drawing {
 	private Shape shape;
