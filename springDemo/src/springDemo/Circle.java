ddb059f4e074d63dc44deb7699c49e54d2ab25ce
JamesJi9277 <jiqi@gwu.edu>
2016-03-10 13:37:30 -0500

    update

--- springDemo/src/springDemo/Circle.java
+++ springDemo/src/springDemo/Circle.java
@@ -1,6 +1,7 @@
 package springDemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 //import org.springframework.beans.factory.annotation.Required;
 
 public class Circle implements Shape{
@@ -11,6 +12,7 @@ public class Circle implements Shape{
 	}
 //	@Required
 	@Autowired
+	@Qualifier("circleRelated")
 	public void setCenter(Point center) {
 		this.center = center;
 	}
