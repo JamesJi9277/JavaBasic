e360790643e0476bb89d01156790e47ac95f7e3a
JamesJi9277 <jiqi@gwu.edu>
2016-03-16 15:06:43 -0400

    update

--- Spring_AOP/src/qiji/AppMain.java
+++ Spring_AOP/src/qiji/AppMain.java
@@ -12,7 +12,14 @@ public class AppMain {
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
 		//get the shapeService this bean, and this bean is belong to the following class
 	    ShapeService shapeService = ctx.getBean("shapeService", ShapeService.class);
-	    System.out.println(shapeService.getCircle().getName());
+	    /*
+	     * 先在spring.xml里面声明一个shapeService的bean
+	     * 然后不用new，而是直接通过getBean声明一个新的类
+	     * 之后因为在implement这个shapeService bean的时候，自动选择了
+	     * autowired="byName"，就自动和triangle和circle匹配了*/
+	    
+	    System.out.println(shapeService.getCircle().getName());//use this flow to write an aspect
+	    
 	}
 
 }
