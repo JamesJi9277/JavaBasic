9a56b8cd923640aea3cf0529d018997b740ce483
JamesJi9277 <jiqi@gwu.edu>
2016-03-20 11:11:43 -0400

    learn more about Pointcut

--- Spring_AOP/src/qiji/aspect/LoggingAspect.java
+++ Spring_AOP/src/qiji/aspect/LoggingAspect.java
@@ -2,6 +2,7 @@ package qiji.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class LoggingAspect {
@@ -25,8 +26,23 @@ public class LoggingAspect {
 	//因为我存在着getCircle返回的是一个circle，所以我在返回的类型上一样要设置成*
 	//如果对于函数里面的参数，arguments，不能够用*，因为*代表一定有东西，这样就忽略了没有arguments的情况
 	//所以这个时候需要用..来代替，..的意思是既可以是任意字符又可以是什么都没有
-	@Before("execution(* qiji.model.*.get*(..))") 
+	@Before("allGetters()") 
 	public void loggingAdvice() {
 		System.out.println("Advice run. Get Method called");
+	}
+	@Before("allCircleGetters()") 
+	public void secondAdvice() {
+		System.out.println("Second Advice run. Get Method called");
+	}
+	//如果有很多这样需要去用到before annotation的，并且结构都相似，那么可以用Pointcut
+	
+	@Pointcut("execution(* get*(..))")
+	public void allGetters() {
+		//allGetters is a dummy method, it holds a Pointcut expression
+	}
+	@Pointcut("within(qiji.model..*)")//more readable
+	//@Pointcut("within(qiji.model.Circle)")代表只跟Circle有关
+	public void allCircleGetters() {
+		
 	}
 }
