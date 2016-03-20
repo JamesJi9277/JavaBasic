c2591cbd8345dda41f09742fdb6d7f9fec9e69b2
JamesJi9277 <jiqi@gwu.edu>
2016-03-20 14:46:44 -0400

    Pointcut

--- Spring_AOP/src/qiji/aspect/LoggingAspect.java
+++ Spring_AOP/src/qiji/aspect/LoggingAspect.java
@@ -1,5 +1,6 @@
 package qiji.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -26,11 +27,15 @@ public class LoggingAspect {
 	//因为我存在着getCircle返回的是一个circle，所以我在返回的类型上一样要设置成*
 	//如果对于函数里面的参数，arguments，不能够用*，因为*代表一定有东西，这样就忽略了没有arguments的情况
 	//所以这个时候需要用..来代替，..的意思是既可以是任意字符又可以是什么都没有
-	@Before("allGetters()") 
-	public void loggingAdvice() {
+	@Before("allGetters() && allCircleGetters()") 
+	//这个代表需要用时满足两个条件才能够执行这个logging advice语句
+	public void loggingAdvice(JoinPoint joinPoint) {
+		//can use joinPoint to get information that trigger the below function
+		//如果我只需要allCircleGetters。那么不管是getter还是setter我都会print
+		//如果用到了joinPoint就可以避免这个问题
 		System.out.println("Advice run. Get Method called");
 	}
-	@Before("allCircleGetters()") 
+	@Before("allGetters()") 
 	public void secondAdvice() {
 		System.out.println("Second Advice run. Get Method called");
 	}
@@ -40,9 +45,11 @@ public class LoggingAspect {
 	public void allGetters() {
 		//allGetters is a dummy method, it holds a Pointcut expression
 	}
-	@Pointcut("within(qiji.model..*)")//more readable
+	@Pointcut("within(qiji.model..*)")//more readable,all method within this class
 	//@Pointcut("within(qiji.model.Circle)")代表只跟Circle有关
 	public void allCircleGetters() {
 		
 	}
+	//can combine all getters and all setters
+	//@Pointcut(args())
 }
